a30ec81ea930544484ed8c8fb3249ed7133ab812
Cameron Salisbury <cam2772@gmail.com>
2021-05-04 13:59:56 +1200

    Transformed some syntax to others to reduce number of rules

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ cd $scriptdir
 if [[ "$@" != *-ni* ]]
 then
     echo "Compiling Interpreter..."
-    kompile -d "./out/interpreter" "./src/Interpreter/small-interpreter.k" "--gen-glr-bison-parser" "--syntax-module" "SMALL-SYNTAX" $@
+    kompile -d "./out/interpreter" "./src/Interpreter/small-interpreter.k" "--gen-glr-bison-parser" "--syntax-module" "SMALL-SYNTAX-BASE" $@
 fi
 if [[ "$@" != *-nt* ]]
 then
     echo "Compiling Type Checker..."
-    kompile -d "./out/type-checker" "./src/TypeChecker/small-type-checker.k" "--gen-glr-bison-parser" "--syntax-module" "SMALL-SYNTAX" $@
+    kompile -d "./out/type-checker" "./src/TypeChecker/small-type-checker.k" "--gen-glr-bison-parser" "--syntax-module" "SMALL-SYNTAX-BASE" $@
 fi
